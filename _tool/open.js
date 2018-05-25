122156040a4d37b68af9ed5eb9ce01d140d4ca02
jintaixizhao <baizita@163.com>
2018-05-25 09:16:22 +0800

    另起服务，实现点击编辑按钮就打开本地文件

---
+++ _tool/open.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+const http = require('http');
+const path = require("path");
+const rootPath = path.resolve(__dirname,'../')
+const port = 3001
+
+http.createServer((req,res)=>{
+	const reqUrl = decodeURI(req.url).split("=").pop()
+	exec(rootPath+"/"+reqUrl)
+}).listen(port, function () {
+    console.log('WebServer running at http://127.0.0.1:'+port+'/');
+});
+
+
