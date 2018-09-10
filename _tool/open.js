e1ae6c08849578eb9162353d27c2efcc42ba4ed1
jintaixizhao <baizita@163.com>
2018-09-10 09:25:41 +0800

    modify some files

--- _tool/open.js
+++ _tool/open.js
@@ -5,7 +5,7 @@ const rootPath = path.resolve(__dirname,'../')
 const port = 3001
 
 http.createServer((req,res)=>{
-	const reqUrl = decodeURI(req.url).split("=").pop()
+	const reqUrl = decodeURI(req.url).split("=").pop();console.log("打开"+rootPath+"/"+reqUrl);
 	exec(rootPath+"/"+reqUrl)
 }).listen(port, function () {
 	console.log('WebServer running at http://127.0.0.1:'+port+'/');
